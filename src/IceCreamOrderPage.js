79af619ec2f449f1a4f84a53d99c5619378e9cad
github username <github email address>
2019-09-19 17:23:47 -0400

    fixing routes

---
+++ src/IceCreamOrderPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class IceCreamOrderPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Order ice cream here</h1>
+      </div>
+    );
+  }
+}
+
+export default IceCreamOrderPage;
